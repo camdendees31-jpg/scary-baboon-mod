6083a390ee5442603c61907c302f9ec334634f7d
camdendees31-jpg <camdendees31@gmail.com>
2026-04-18 08:47:59 -0500

    Update main.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "logger.hpp"
-#include "beatsaber-hook/shared/utils/hooking.hpp"
-#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "hooking.hpp"
+#include "il2cpp-utils.hpp"
 
 static constexpr auto Logger = Paper::ConstLoggerContext("scary-baboon-mod");
