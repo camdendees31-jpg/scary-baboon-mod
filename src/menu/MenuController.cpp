d16ded390e7c246cedd0f8d76c44140a2a258cc0
camdendees31-jpg <camdendees31@gmail.com>
2026-04-18 08:30:29 -0500

    Update MenuController.cpp

--- src/menu/MenuController.cpp
+++ src/menu/MenuController.cpp
@@ -1,13 +1,5 @@
 #include "MenuController.hpp"
-#include "logger.hpp"
-#include "GlobalNamespace/OVRInput.hpp"
-#include "UnityEngine/Time.hpp"
-#include "UnityEngine/Object.hpp"
-#include "UnityEngine/Canvas.hpp"
-#include "UnityEngine/RenderMode.hpp"
-#include "UnityEngine/RectTransform.hpp"
-#include "UnityEngine/Vector2.hpp"
-#include "UnityEngine/Vector3.hpp"
+#include "main.hpp"
 
 ModFeatures g_features;
 ScaryBaboonMod::MenuController* ScaryBaboonMod::MenuController::instance = nullptr;
@@ -18,27 +10,14 @@ void ScaryBaboonMod::MenuController::Awake() {
     instance = this;
     isVisible = false;
     yHoldTimer = 0.0f;
-    BuildMenu();
-    HideMenu();
 }
 
 void ScaryBaboonMod::MenuController::Update() {
-    bool yPressed = GlobalNamespace::OVRInput::Get(
-        GlobalNamespace::OVRInput::Button::Two,
-        GlobalNamespace::OVRInput::Controller::LTouch
-    );
-    if (yPressed) {
-        yHoldTimer += UnityEngine::Time::get_deltaTime();
-        if (yHoldTimer >= HOLD_THRESHOLD) { yHoldTimer = 0.0f; ToggleMenu(); }
-    } else { yHoldTimer = 0.0f; }
-    UnityEngine::Time::set_timeScale(g_features.freezeTimer ? 0.0f : g_features.timeScale);
+    // Y-hold to toggle menu - OVR hook goes here once codegen is available
+    yHoldTimer = 0.0f;
 }
 
-void ScaryBaboonMod::MenuController::ShowMenu() { if (menuRoot) menuRoot->SetActive(true); isVisible = true; }
-void ScaryBaboonMod::MenuController::HideMenu() { if (menuRoot) menuRoot->SetActive(false); isVisible = false; }
+void ScaryBaboonMod::MenuController::ShowMenu()  { isVisible = true; }
+void ScaryBaboonMod::MenuController::HideMenu()  { isVisible = false; }
 void ScaryBaboonMod::MenuController::ToggleMenu() { isVisible ? HideMenu() : ShowMenu(); }
-
-void ScaryBaboonMod::MenuController::BuildMenu() {
-    menuRoot = UnityEngine::GameObject::New_ctor(StringW("SBModMenu"));
-    UnityEngine::Object::DontDestroyOnLoad(menuRoot);
-}
+void ScaryBaboonMod::MenuController::BuildMenu()  { }
